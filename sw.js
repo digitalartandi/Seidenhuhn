4d2dc3788f17931658fd7147386fa88bc3ce201e
digitalartandi <digital.art.andi@gmail.com>
2025-08-09 14:15:17 +0200

    Add files via upload
    D

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('seidenhuhn-v7').then(c => c.addAll(['./','./seidenhuhn.html','./manifest.webmanifest','./cover.webp','./hen.webp'])));
+  e.waitUntil(caches.open('seidenhuhn-v7a').then(c => c.addAll(['./','./seidenhuhn.html','./manifest.webmanifest','./cover.webp','./hen.webp'])));
 });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
