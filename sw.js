dff9509ac9b32bc9578709870a3e3e815b7a8ffb
digitalartandi <digital.art.andi@gmail.com>
2025-08-10 00:03:34 +0200

    Add files via upload
    Xf

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('seidenhuhn-v1110').then(c => c.addAll(['./','./seidenhuhn.html','./manifest.webmanifest'])));
-});
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+// Service Worker Code hier
